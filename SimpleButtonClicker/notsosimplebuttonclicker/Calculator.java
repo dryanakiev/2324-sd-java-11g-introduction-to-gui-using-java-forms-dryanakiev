2f4bccf6c8ade70e59771840d095d8680d8d21e0
Димитър Янакиев <dyanakiev@codingburgas.bg>
2024-01-04 09:21:31 +0200

    Add calculator design

---
+++ SimpleButtonClicker/notsosimplebuttonclicker/Calculator.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Calculator {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton cButton;
+    private JButton button3;
+    private JButton a7Button;
+    private JButton xButton;
+    private JButton a9Button;
+    private JButton cButton1;
+    private JButton a8Button;
+    private JButton a4Button;
+    private JButton a0Button;
+    private JButton a6Button;
+    private JButton a5Button;
+    private JButton a1Button;
+    private JButton button14;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton button17;
+    private JButton a0Button1;
+    private JButton button19;
+    private JButton button20;
+}
